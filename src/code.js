6cc3b6af20785a5e3ac9c72cd5325018e2e99eaa
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-12-23 17:57:58 +0500

    Показ выбранного текста при запуске плагина

--- src/code.js
+++ src/code.js
@@ -1,8 +1,14 @@
 figma.showUI(__html__, {width: 760, height: 600});
 
-figma.on("selectionchange", () => {
+function getSelectedText() {
     let selection = figma.currentPage.selection;
-    if (selection && selection[0].type === 'TEXT') {
+    if (selection.length > 0 && selection[0].type === 'TEXT') {
         figma.ui.postMessage({action: 'showTxt', text: selection[0].characters });
     }
+}
+
+figma.on("selectionchange", () => {
+    getSelectedText();
 })
+
+getSelectedText();
