280b36dbad3e863197ac4dab24b1f6df4c38c784
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-12-23 17:30:33 +0500

    Показ выбранного текста в окне плагина

--- src/code.js
+++ src/code.js
@@ -1,1 +1,8 @@
-figma.showUI(__html__, {width: 760, height: 600})
+figma.showUI(__html__, {width: 760, height: 600});
+
+figma.on("selectionchange", () => {
+    let selection = figma.currentPage.selection;
+    if (selection && selection[0].type === 'TEXT') {
+        figma.ui.postMessage({action: 'showTxt', text: selection[0].characters });
+    }
+})
