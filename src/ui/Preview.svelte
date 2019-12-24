4a5e9ae0756f774707bd316a58b69fcf342291f9
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-12-24 17:20:17 +0500

    Выделение проблемных фрагментов в тексте

--- src/ui/Preview.svelte
+++ src/ui/Preview.svelte
@@ -1,6 +1,21 @@
 <script>
     export let text;
-    let formated;
+    export let fragments;
+
+    function showProofread(list, txt) {
+        let result = "";
+        let position = 0;
+        
+        for (let i = 0; i < list.length; i++) {
+            let item = list[i];
+            result = result + txt.slice(position, item['start']);
+            result = result + '<span class="proof--underline">' + txt.slice(item['start'], item['end']) + '</span>';
+            position =  item['end'];
+        }
+        result = result + txt.slice(position, txt.length);
+        return result;
+    }
+    $: text = fragments ? showProofread(fragments, text) : text;
 
     function formatTxt(txt) {
         return '<p>' + txt.replace(/(?:\r\n|\r|\n)/g, '<p/><p>') + '</p>';
@@ -18,12 +33,12 @@
         font-size: 14px;
         line-height: 24px;
     }
-    .highlighted {
+    :global(.proof--highlighted) {
         color: #da570f;
         background: #fdc;
         border-radius: .2em;
     }
-    .underline {
+    :global(.proof--underline) {
         color: #dA570f;
         background: url(../../assets/img/underline.svg) 0 100% repeat-x;
 
@@ -31,7 +46,7 @@
         padding: 3px 2px;
         margin: 0 -2px;
     }
-    .active {
+    :global(.proof--active) {
         -webkit-transition: background-color 0s, color 0s;
         color: #fff;
         background: #DA570F;
