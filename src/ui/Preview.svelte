1b30e68f571826a547a8bf34f75f58d8957537cf
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-12-24 10:20:21 +0500

    Базовые стили для подсветки стоп-слов

--- src/ui/Preview.svelte
+++ src/ui/Preview.svelte
@@ -18,6 +18,24 @@
         font-size: 14px;
         line-height: 24px;
     }
+    .highlighted {
+        color: #da570f;
+        background: #fdc;
+        border-radius: .2em;
+    }
+    .underline {
+        color: #dA570f;
+        background: url(../../assets/img/underline.svg) 0 100% repeat-x;
+
+        border-radius: .2em;
+        padding: 3px 2px;
+        margin: 0 -2px;
+    }
+    .active {
+        -webkit-transition: background-color 0s, color 0s;
+        color: #fff;
+        background: #DA570F;
+    }
 </style>
 
 <div class="preview">
